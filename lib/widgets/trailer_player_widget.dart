3ac191a9e19f0c44be859a4200e03e1290e0d8ea
Sjoze1 <jojorichard.ochieng@gmail.com>
2025-07-01 18:22:21 +0300

    lanscape mode fix

--- lib/widgets/trailer_player_widget.dart
+++ lib/widgets/trailer_player_widget.dart
@@ -3,6 +3,7 @@ import 'package:flutter/foundation.dart' show kIsWeb;
 import 'package:video_player/video_player.dart';
 import 'package:chewie/chewie.dart';
 import 'dart:html' as html;
+import 'package:flutter/services.dart';
 
 import '../widgets/end_screen_widget.dart';
 
@@ -31,7 +32,6 @@ class _TrailerPlayerWidgetState extends State<TrailerPlayerWidget> {
   @override
   void initState() {
     super.initState();
-    if (kIsWeb) _enterFullscreen();
     _initializePlayer(widget.trailerUrl);
   }
 
@@ -41,34 +41,77 @@ class _TrailerPlayerWidgetState extends State<TrailerPlayerWidget> {
       videoPlayerOptions: VideoPlayerOptions(mixWithOthers: true),
     );
     await _videoController.initialize();
+
     _chewieController?.dispose();
     _chewieController = ChewieController(
       videoPlayerController: _videoController,
       autoPlay: true,
       looping: false,
-      allowFullScreen: false,
+      allowFullScreen: true,
       showControls: true,
       aspectRatio: _videoController.value.aspectRatio,
     );
+
     _videoController.addListener(() {
       if (_videoController.value.position >= _videoController.value.duration &&
           !_videoController.value.isPlaying &&
           mounted &&
           !_showEndScreen) {
         setState(() => _showEndScreen = true);
+        _exitFullscreenAndUnlockOrientation();
       }
     });
+
+    if (kIsWeb) {
+      html.document.onFullscreenChange.listen((event) {
+        if (html.document.fullscreenElement != null) {
+          _lockOrientationToLandscape();
+        } else {
+          _unlockOrientation();
+        }
+      });
+    }
+
     setState(() => _isInitializing = false);
   }
 
-  void _enterFullscreen() {
-    final doc = html.document.documentElement;
-    if (doc?.requestFullscreen != null) doc!.requestFullscreen();
+  void _lockOrientationToLandscape() {
+    if (kIsWeb) {
+      try {
+        html.window.screen?.orientation?.lock('landscape').catchError((e) {
+          print('Error locking orientation to landscape: $e');
+        });
+      } catch (e) {
+        print('Could not access screen orientation API: $e');
+      }
+    }
+  }
+
+  void _unlockOrientation() {
+    if (kIsWeb) {
+      try {
+        html.window.screen?.orientation?.unlock();
+      } catch (e) {
+        print('Could not access screen orientation API for unlock: $e');
+      }
+    }
+  }
+
+
+  void _exitFullscreenAndUnlockOrientation() {
+    if (kIsWeb) {
+      try {
+        html.document.exitFullscreen();
+        _unlockOrientation();
+      } catch (error) {
+        print('Error exiting fullscreen: $error');
+      }
+    }
   }
 
   @override
   void dispose() {
-    if (kIsWeb) html.document.exitFullscreen();
+    _exitFullscreenAndUnlockOrientation();
     _videoController.dispose();
     _chewieController?.dispose();
     super.dispose();
@@ -76,46 +119,31 @@ class _TrailerPlayerWidgetState extends State<TrailerPlayerWidget> {
 
   @override
   Widget build(BuildContext context) {
-    final sz = MediaQuery.of(context).size;
-    final isLandscape = sz.width > sz.height;
-
-    Widget player = _isInitializing || _chewieController == null
-        ? const Center(child: CircularProgressIndicator())
-        : Stack(
-            fit: StackFit.expand,
-            children: [
-              Chewie(controller: _chewieController!),
-              if (_showEndScreen)
-                EndScreenWidget(onWatchFull: widget.onWatchFull),
-              Positioned(
-                top: 10,
-                right: 10,
-                child: IconButton(
-                  icon: const Icon(Icons.close, color: Colors.white, size: 24),
-                  onPressed: widget.onExit,
+    return Scaffold(
+      body: _isInitializing || _chewieController == null
+          ? const Center(child: CircularProgressIndicator())
+          : Stack(
+              fit: StackFit.expand,
+              children: [
+                GestureDetector(
+                  child: Chewie(controller: _chewieController!),
+                  onTap: () {},
                 ),
-              ),
-            ],
-          );
-
-    if (kIsWeb && !isLandscape) {
-      player = RotatedBox(
-        quarterTurns: 1,
-        child: SizedBox(
-          width: sz.height,
-          height: sz.width,
-          child: FittedBox(
-            fit: BoxFit.cover,
-            child: SizedBox(
-              width: sz.width,
-              height: sz.height,
-              child: player,
+                if (_showEndScreen)
+                  EndScreenWidget(onWatchFull: widget.onWatchFull),
+                Positioned(
+                  top: 10,
+                  right: 10,
+                  child: IconButton(
+                    icon: const Icon(Icons.close, color: Colors.white, size: 24),
+                    onPressed: () {
+                      _exitFullscreenAndUnlockOrientation();
+                      widget.onExit();
+                    },
+                  ),
+                ),
+              ],
             ),
-          ),
-        ),
-      );
-    }
-
-    return Scaffold(body: player);
+    );
   }
 }
