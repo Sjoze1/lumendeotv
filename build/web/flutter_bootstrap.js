af8f3cd9890b493b9d9026452867c25eec7f2312
Sjoze1 <jojorichard.ochieng@gmail.com>
2025-07-02 14:19:53 +0300

    Force add built web for deployment

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"18818009497c581ede5d8a3b8b833b81d00ceb
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4150583961"
+    serviceWorkerVersion: "4240300715"
   }
 });
